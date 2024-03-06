647c79d294d5dd866932ce57009a8c303f24a8fc
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-03-06 15:42:55 +0900

    Create 추억점수.cpp

---
+++ Programmers/추억 점수/추억점수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+// https://school.programmers.co.kr/learn/courses/30/lessons/176963
+
+// 사진 별 추억 점수 저장하는 맵
+map <string, int> scoreMaps;
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    
+    // 사진 별 추억 점수를 맵에 저장
+    for(int i = 0; i < name.size(); i++){
+        scoreMaps[name[i]] = yearning[i];
+    }        
+    
+    // 점수 산출
+    for(int i =0; i<photo.size(); i++){
+        int score = 0;
+        for(int j = 0; j < photo[i].size(); j++){
+            score += scoreMaps[photo[i][j]];
+        }
+        answer.push_back(score);
+    }
+    
+    return answer;
+}
