15ea7e7560d04ba958d304d3a4953569858b4197
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-03-18 17:27:44 +0900

    Update 콜라 문제.cpp

--- Programmers/콜라 문제/콜라 문제.cpp
+++ Programmers/콜라 문제/콜라 문제.cpp
@@ -7,15 +7,14 @@ int solution(int a, int b, int n) {
     int answer = 0, coke = 0 , emptyCoke= 0;
     
     while(true){
-        emptyCoke = n; // 다 마신 콜라 갯수        
+        emptyCoke = n;  // 빈 콜라 병 갯수 저장      
         coke = (emptyCoke / a) * b; // 마트에서 받은 콜라 갯수
-        emptyCoke %= a; // 마트에 가져가지 않은 빈 콜라 갯수
-        
-        answer += coke; // 받은 콜라 갯수 ++
+        answer += coke; // 받은 콜라 ++
+        emptyCoke %= a; // 마트에 전달하지 않은 빈 콜라 병 갯수
         
         n = emptyCoke + coke; // 현재 가지고 있는 콜라 갯수
-    
-        if(n / a == 0) {break;} // 더이상 받을 게 없다면
+               
+        if(n / a == 0) {break; } // 더 이상 마트에 전달할 콜라가 없다면 break;
     }
     return answer;
 }
