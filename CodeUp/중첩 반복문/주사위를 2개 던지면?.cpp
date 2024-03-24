4fbfda3e058441bfbe18e99308ac3501ebfc7700
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-03-24 20:01:24 +0900

    Update 주사위를 2개 던지면?.cpp

--- CodeUp/중첩 반복문/주사위를 2개 던지면?.cpp
+++ CodeUp/중첩 반복문/주사위를 2개 던지면?.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
 
 	int n, m;
-
+	// 주사위 면의 갯수 입력
 	cin >> n >> m;
-
+	// 모든 경우의 수 구하기
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
 			cout << i << " " << j << endl;
