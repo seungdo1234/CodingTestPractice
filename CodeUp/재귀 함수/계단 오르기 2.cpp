993a4394937b86c8fb1fc6b258fd95f0583d930d
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-04-06 16:46:15 +0900

    Update 계단 오르기 2.cpp

--- CodeUp/재귀 함수/계단 오르기 2.cpp
+++ CodeUp/재귀 함수/계단 오르기 2.cpp
@@ -1,3 +1,4 @@
+// 재귀 함수
 #include <iostream>
 #include <vector>
 // https://codeup.kr/problem.php?id=3704&rid=0
@@ -31,3 +32,38 @@ int main() {
 
     return 0;
 }
+//-----------------------------------------------------------------------------------
+// 반복문
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MOD = 1000; // 결과를 1000으로 나눈 나머지를 저장하기 위한 상수
+
+int countWays(int n) {
+    // dp[i]는 i개의 계단을 오를 때 가능한 경우의 수를 저장하는 배열
+    vector<int> dp(n + 1, 0);
+    
+    // 초기값 설정
+    dp[0] = 1; // 0계단을 오르는 경우는 한 가지 방법이 있다.
+    dp[1] = 1; // 1계단을 오르는 경우도 한 가지 방법이 있다.
+    dp[2] = 2; // 2계단을 오르는 경우는 두 가지 방법이 있다. (1계단씩 오르거나, 2계단을 한 번에 오르거나)
+
+    // 점화식을 이용하여 dp 배열 완성
+    for (int i = 3; i <= n; ++i) {
+        dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % MOD;
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ways = countWays(n);
+    cout << ways << endl;
+
+    return 0;
+}
+
