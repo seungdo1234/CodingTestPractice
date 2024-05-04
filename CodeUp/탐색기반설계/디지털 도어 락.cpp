cbba230dbb0b2a6536dc0958f4ee7867d25013ec
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-05 00:34:39 +0900

    Create 디지털 도어 락.cpp

---
+++ CodeUp/탐색기반설계/디지털 도어 락.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+// https://codeup.kr/problem.php?id=2650
+using namespace std;
+
+// 유클리드 호제법
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int key1, key2, key3;
+
+    // 3개의 내부 보안키 값을 입력 받는다.
+    cin >> key1 >> key2 >> key3;
+
+    // 첫 번째와 두 번째 수의 최대공약수를 구한다.
+    int gcd1 = gcd(key1, key2);
+
+    // 위에서 구한 최대공약수와 세 번째 수의 최대공약수를 구한다.
+    int master_key_id = gcd(gcd1, key3);
+
+    // 계산된 값(최대공약수)을 출력한다.
+    cout << master_key_id << endl;
+
+    return 0;
+}
