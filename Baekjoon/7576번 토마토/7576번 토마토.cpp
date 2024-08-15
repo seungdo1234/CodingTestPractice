51a86e64b6a86a8e6c994a5ea48e0d431bdef1af
Ji Seung Do <86179438+seungdo1234@users.noreply.github.com>
2024-08-16 00:17:58 +0900

    Update 7576번 토마토.cpp

--- Baekjoon/7576번 토마토/7576번 토마토.cpp
+++ Baekjoon/7576번 토마토/7576번 토마토.cpp
@@ -1,3 +1,92 @@
+// 수정 후 코드
+#include <iostream>
+#include <vector>
+#include <queue>
+
+// https://www.acmicpc.net/problem/7576
+
+using namespace std;
+
+int m, n;
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+
+vector<vector<int>> map;
+queue <pair<int, int>> q;
+
+void BFS()
+{
+	while (!q.empty()) {
+		pair<int,int > front = q.front();
+
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int moveY = front.first + dy[i];
+			int moveX = front.second + dx[i];
+
+			if (moveY < 0 || moveY >= n || moveX < 0 || moveX >= m)
+				continue;
+
+			if (map[moveY][moveX] == 0) {
+				map[moveY][moveX] = map[front.first][front.second] + 1;
+				q.push({ moveY,moveX });
+			}
+		}
+	}
+}
+void StartSearch() {
+	int result = 1;
+	BFS();
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (map[i][j] == 0) {
+				cout << "-1\n";
+				return;
+			}
+
+			if (result < map[i][j]) {
+				result = map[i][j];
+			}
+		}
+	}
+	
+	cout << result - 1 << "\n";
+
+}
+int main() {
+	vector<pair<int, int>> initVec;
+
+	cin >> m >> n;
+
+	for (int i = 0; i < n; i++) {
+		int sel;
+		vector<int> vec;
+		for (int j = 0; j < m; j++) {
+			cin >> sel;
+			vec.push_back(sel);
+
+			if (sel == 1)
+			{
+				q.push({ i,j });
+			}
+		}
+		map.push_back(vec);
+	}
+
+	StartSearch();
+
+	return 0;
+}
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------
+// 수정 전 코드
 #include <iostream>
 #include <vector>
 #include <queue>
