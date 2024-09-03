f396cbf9f8e433d83fa8d85f7dfe7b4cae02d3ba
Ji Seung Do <86179438+seungdo1234@users.noreply.github.com>
2024-09-03 11:46:30 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.33 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -1,48 +1,9 @@
 #include <string>
 #include <vector>
-#include<algorithm>
-#include<iostream>
 
 using namespace std;
-bool cmp_HighY (pair<int,int> a, pair<int,int> b){
-    return a.first > b.first;
-}
-
-bool cmp_HighX (pair<int,int> a, pair<int,int> b){
-    return a.second > b.second;
-}
-
-bool cmp_LowX (pair<int,int> a, pair<int,int> b){
-    return a.second < b.second;
-}
 
 vector<int> solution(vector<string> wallpaper) {
     vector<int> answer;
-    vector <pair<int, int>> fileVec;
-    
-    for(int i = 0; i < wallpaper.size(); i++){
-        for(int j = 0; j < wallpaper[i].length(); j++){
-            if(wallpaper[i][j] == '#')
-                fileVec.push_back({i, j});
-        }
-    }
-    
-    sort(fileVec.begin(), fileVec.end());
-    answer.push_back(fileVec[0].first);
-    
-        sort(fileVec.begin(), fileVec.end(), cmp_LowX);
-
-     answer.push_back(fileVec[0].second);
-    
-
-            sort(fileVec.begin(), fileVec.end(),cmp_HighY);
-    
-         answer.push_back(fileVec[0].first + 1);
-    
-       sort(fileVec.begin(), fileVec.end(), cmp_HighX);
-    
-         answer.push_back(fileVec[0].second + 1);
-    
-
     return answer;
 }
