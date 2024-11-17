0816b847ae6cca28b1b67318bd95a80411d44532
Ji Seung Do <86179438+seungdo1234@users.noreply.github.com>
2024-11-17 19:09:17 +0900

    [level 2] Title: 롤케이크 자르기, Time: 54.50 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,40 +1,23 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+
 using namespace std;
 
 unordered_map<int,int> leftCut;
 unordered_map<int,int> rightCut;
 
-
 int solution(vector<int> topping) {
-    int answer = 0;
-
-    if(topping.size() == 1){
-        return answer;
-    }
+    int answer = 0, leftCount = 0, rightCount = 0;
     
-    int leftCount = 0, rightCount = 0;
-    for(int i  = 0; i < topping.size(); i++){
-        if(rightCut[topping[i]]++ == 0){
-            rightCount ++;
-        }
-    }
+    for(int i  = 0; i < topping.size(); i++)
+        if(rightCut[topping[i]]++ == 0) rightCount ++;
     
-    for(int i  = 0; i < topping.size(); i++){
-         if(leftCut[topping[i]]++ == 0){
-            leftCount ++;
-        }   
-        
-       if(--rightCut[topping[i]] == 0){
-            rightCount --;
-        }
-        
-        if(leftCount == rightCount){
-            answer++;
-        }
+    for(int i  = 0; i < topping.size() - 1; i++){
+        if(leftCut[topping[i]]++ == 0) leftCount ++;
+        if(--rightCut[topping[i]] == 0) rightCount --; 
+        if(leftCount == rightCount) answer++;
     }
-    
     
     return answer;
 }
