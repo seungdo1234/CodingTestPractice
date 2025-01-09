8d2faadba0bdd5b95d4a27a7b87119eac19271a8
Ji Seung Do <86179438+seungdo1234@users.noreply.github.com>
2025-01-09 17:28:32 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string input;
+	cin >> input;
+
+	int sum = 0, bar = 0;
+	bool flag = false;
+	stack<char> stack;
+	stack.push('0');
+	for(int i = 0; i < input.length(); i++)
+	{
+		char c = input[i];
+		if(c == '(')
+		{
+			bar ++;
+			stack.push(c);
+			flag = true;
+		}
+		else if(stack.top() == '(' && c == ')')
+		{
+			bar--;
+			if(flag)
+			{
+				sum += bar;
+				flag = false;
+			}
+			else
+			{
+				sum ++;
+			}
+			stack.pop();
+		}
+
+	}
+
+	cout << sum ;
+	return 0;
+}
